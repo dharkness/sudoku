1da1295cc4534c01eb9dc23eb7e2a6460a1b0832
David Harkness <dharkness@gmail.com>
2022-08-13 16:07:28 -0700

    Add `solveXWings` solver

--- src/models/test.ts
+++ src/models/test.ts
@@ -25,6 +25,7 @@ import solveNakedPairs from "../solvers/solveNakedPairs";
 import solveNakedTriples from "../solvers/solveNakedTriples";
 import solveHiddenPairs from "../solvers/solveHiddenPairs";
 import solveHiddenTriples from "../solvers/solveHiddenTriples";
+import solveXWings from "../solvers/solveXWings";
 
 const EMPTY =
   "......... ......... ......... ......... ......... ......... ......... ......... .........";
@@ -58,10 +59,18 @@ const EMPTY =
 //
 // ========== NEEDS NAKED/HIDDEN PAIRS/TRIPLES ========================================
 
+// const start =
+//   "9........ 3...6..2. ..5...7.3 .31.84... 82..1.549 .4....8.. 75.1.6.8. 4..8..1.. ...7.....";
+// const full =
+//   "962378415 374561928 185429763 531984672 827613549 649257831 753196284 496832157 218745396";
+
+//
+// ========== NEEDS X-WING ========================================
+
 const start =
-  "9........ 3...6..2. ..5...7.3 .31.84... 82..1.549 .4....8.. 75.1.6.8. 4..8..1.. ...7.....";
+  "1.....569 492.561.8 .561.924. ..964.8.1 .64.1.... 218.356.4 .4.5...16 9.5.614.2 621.....5";
 const full =
-  "962378415 374561928 185429763 531984672 827613549 649257831 753196284 496832157 218745396";
+  "187423569 492756138 356189247 539647821 764218953 218935674 843592716 975361482 621874395";
 
 // const state = createEmptySimpleState();
 // for (const [p, k] of [
@@ -90,6 +99,7 @@ while (!stop && (knowns.length || pencils.length)) {
   solveNakedTriples(state, solved);
   solveHiddenPairs(state, solved);
   solveHiddenTriples(state, solved);
+  solveXWings(state, solved);
   if (!solved.isEmpty()) {
     // console.info("FOUND", newSolutions);
     // nakeds do not solve knowns directly
---
+++ src/solvers/solveXWings.ts
@@ -0,0 +1,123 @@
+import { ReadableState } from "../models/state";
+import { Solutions } from "../models/solutions";
+import { BOARD, Cell, Group } from "../models/structure";
+import { ALL_KNOWNS, Coord, Grouping } from "../models/basics";
+import { difference } from "../utils/collections";
+import { printCellPossibles } from "../models/printers";
+
+const LOG = false;
+
+/**
+ * Looks for X-Wings in rows and columns to determine pencil marks to remove.
+ * Removes found known from other cells in opposite row/column.
+ *
+ * #xample: This grid of 4-possibles shows an X-Wing in rows 2 and 5
+ *
+ *        ↓   ↓
+ *     123456789
+ *   1 ·········
+ *   2 ···4···4·
+ *   3 ·········
+ *   4 ·········
+ * → 5 ···4···4·
+ * → 6 ·········
+ *   7 ···4·····  ←-- remove 4 from cell 74
+ *   8 ·······4·  ←-- remove 4 from cell 88
+ *   9 ·········
+ */
+export default function solveXWings(
+  state: ReadableState,
+  solutions: Solutions
+): void {
+  const iterations = [
+    [Grouping.ROW, BOARD.rows, Grouping.COLUMN, BOARD.columns],
+    [Grouping.COLUMN, BOARD.columns, Grouping.ROW, BOARD.rows],
+  ] as [Grouping, Map<Coord, Group>, Grouping, Map<Coord, Group>][];
+
+  for (const [g1, groups1, g2, groups2] of iterations) {
+    for (const k of ALL_KNOWNS) {
+      for (const [i1, group1] of groups1) {
+        const cells1 = state.getPossibleCells(group1, k);
+        if (cells1.size !== 2) {
+          continue;
+        }
+
+        for (const [i2, group2] of groups1) {
+          if (i2 <= i1) {
+            continue;
+          }
+
+          const cells2 = state.getPossibleCells(group2, k);
+          if (cells2.size !== 2) {
+            continue;
+          }
+
+          const [cell11, cell12] = [...cells1];
+          const [cell21, cell22] = [...cells2];
+          if (
+            cell11!.point.i[g1] !== cell21!.point.i[g1] ||
+            cell12!.point.i[g1] !== cell22!.point.i[g1]
+          ) {
+            continue;
+          }
+
+          const xwing = new Set([cell11, cell12, cell21, cell22]);
+          const erase = new Set<Cell>();
+          for (const otherGroup of [
+            groups2.get(cell11!.point.i[g1])!,
+            groups2.get(cell12!.point.i[g1])!,
+          ]) {
+            const diff = difference(
+              state.getPossibleCells(otherGroup, k),
+              xwing
+            );
+            if (diff.size) {
+              for (const cell of diff) {
+                erase.add(cell!);
+              }
+            }
+          }
+          if (!erase.size) {
+            LOG &&
+              console.info(
+                "empty X-Wing for",
+                k,
+                "in",
+                `${Grouping[g1]}S`,
+                group1.coord + 1,
+                "and",
+                group2.coord + 1,
+                `${Grouping[g2]}S`,
+                cell11!.point.i[g1] + 1,
+                "and",
+                cell12!.point.i[g1] + 1
+              );
+            continue;
+          }
+
+          printCellPossibles(state, k);
+          LOG &&
+            console.info(
+              "SOLVE X-WING for",
+              k,
+              "in",
+              `${Grouping[g1]}S`,
+              group1.coord + 1,
+              "and",
+              group2.coord + 1,
+              `${Grouping[g2]}S`,
+              cell11!.point.i[g1] + 1,
+              "and",
+              cell12!.point.i[g1] + 1,
+              "erase",
+              Cell.stringFromPoints(erase)
+            );
+
+          for (const cell of erase) {
+            solutions.addErasedPencil(cell, k);
+          }
+        }
+      }
+    }
+  }
+}
